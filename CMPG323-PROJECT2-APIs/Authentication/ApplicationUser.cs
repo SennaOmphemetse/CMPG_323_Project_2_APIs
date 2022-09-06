18ee3496b1fc33e3ad3dd78cc5a4a5b4dd38f840
SennaOmphe <90115638+SennaOmphe@users.noreply.github.com>
2022-09-06 02:01:13 +0200

    add updated items

---
+++ CMPG323-PROJECT2-APIs/Authentication/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CMPG323_PROJECT2_APIs.Authentication
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+    }
+}
---
+++ CMPG323-PROJECT2-APIs/Authentication/UserSignUP.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CMPG323_PROJECT2_APIs.Authentication
+{
+    public class UserSignUP
+    {
+        [Required(ErrorMessage = "FirstName required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "LastName required")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "UserName required")]
+        public string Username{get; set;}
+
+        [EmailAddress]
+        [Required(ErrorMessage = "EmailAddress required")]
+        public string Email {get;set;}
+
+
+        [Required(ErrorMessage = "Password required")]
+        public string Password {get; set;}
+
+    }
+}
